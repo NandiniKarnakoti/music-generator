25d6b77ad2949799d2049f239243cf965b2fdb30
NandiniKarnakoti <132219274+NandiniKarnakoti@users.noreply.github.com>
2025-06-16 09:47:30 +0530

    Update musicRoutes.js

--- backend/routes/musicRoutes.js
+++ backend/routes/musicRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createMusic, getAllMusic, deleteMusic } = require('../controllers/musicController');
+const { createMusic, getAllMusic, deleteMusic } = require('../controllers/musicControllers');
 router.post('/', createMusic);
 router.get('/', getAllMusic);
 router.delete('/:id', deleteMusic);
 
 module.exports = router;
+
